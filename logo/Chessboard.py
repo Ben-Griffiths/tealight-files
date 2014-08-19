0f25d2ea43600e164288bee53011f3c060586243
Ben-Griffiths <skulled590@googlemail.com>
2014-08-19 12:05:26 +0100

    Closing logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -2,12 +2,11 @@ from tealight.logo import move, turn
 
 def Chessboard():
   for i in range (0,4):
-     move(80)
-     turn(90)
      if (i % 2) == 0:
-        
+       move(80)
+       turn(90)
      else:
-          
+         
       
       
 Chessboard()    
