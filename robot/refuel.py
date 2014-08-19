7bfe54de1b580b2a684618719dab3f63f5d3c453
Ben-Griffiths <skulled590@googlemail.com>
2014-08-19 14:59:47 +0100

    Running robot/refuel.py

--- robot/refuel.py
+++ robot/refuel.py
@@ -7,3 +7,25 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+while True:
+  move()
+  if(left_side() == "fruit"):
+    turn(-1)
+  elif(right_side() == "fruit"):
+    turn(1)
+  elif (look() == "fruit"):
+    while (look() == "fruit"):
+      move()
+  if(left_side() == None):
+    turn(-1)
+    move()
+   
+  if(touch() == "wall"):
+    if(left_side() != "wall"):
+      turn(-1)
+      move()
+    elif(right_side() != "wall"):
+      turn(1)      
+    else:
+      turn(2)
+  
