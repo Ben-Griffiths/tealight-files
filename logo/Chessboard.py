8d373525d058f1606cc1d1293ff577cee12e27e8
Ben-Griffiths <skulled590@googlemail.com>
2014-08-19 12:10:47 +0100

    Running logo/Chessboard.py

--- logo/Chessboard.py
+++ logo/Chessboard.py
@@ -1,6 +1,13 @@
 from tealight.logo import move, turn
-
+def filled = true:
+def square(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
+def square_filled(side):
+  for i in range(0,4):
+    move(side)
+    turn(90)
+    
 def Chessboard():
-
-      
-Chessboard()    
+  
