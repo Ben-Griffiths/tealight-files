71788e214c1697611936d81e47e7a3c78fef3223
Ben-Griffiths <skulled590@googlemail.com>
2014-08-19 14:51:24 +0100

    Running robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -10,20 +10,20 @@ from tealight.robot import (move,
 while True:
   move()
   if(left_side() == "fruit"):
-    turn(1)
+    turn(-1)
     move()
   elif(right_side() == "fruit"):
-    turn(-1)
+    turn(1)
     move()
   elif (look() == "fruit"):
     while (look() == "fruit"):
       move()
   if(touch() == "wall"):
     if(left_side() != "wall"):
-      turn(1)
+      turn(-1)
       move()
     elif(right_side() != "wall"):
-      turn(-1)
+      turn(1)
       move()
   
 
