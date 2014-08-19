1726d63913b5b0da4cc1425dcd3e80efdff6ef80
Ben-Griffiths <skulled590@googlemail.com>
2014-08-19 14:59:17 +0100

    Closing robot/maze.py

--- robot/maze.py
+++ robot/maze.py
@@ -11,25 +11,21 @@ while True:
   move()
   if(left_side() == "fruit"):
     turn(-1)
-    move()
   elif(right_side() == "fruit"):
     turn(1)
-    move()
   elif (look() == "fruit"):
     while (look() == "fruit"):
       move()
   if(left_side() == None):
     turn(-1)
     move()
-  
-  
+   
   if(touch() == "wall"):
     if(left_side() != "wall"):
       turn(-1)
       move()
     elif(right_side() != "wall"):
-      turn(1)
-      move()
+      turn(1)      
     else:
       turn(2)
   
