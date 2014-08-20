f4d184c29bd90648c9369e067ceb7f66f7ef790c
Ben-Griffiths <skulled590@googlemail.com>
2014-08-20 12:25:40 +0100

    Running art/Connect4.py

--- art/Connect4.py
+++ art/Connect4.py
@@ -1,1 +1,12 @@
-print "A new file!"
+#Create grid
+#Draw grid
+#Play + check legitimacy
+#Win conds
+
+#Timer
+
+Gridarray[7][7]
+
+for i in range (0,7):
+  for j in range (0,7):
+    Gridarray[i][j] = 0
