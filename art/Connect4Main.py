f9d670d994790cdad16b8d0e9575266442810ff1
GriffithsBen <skulled590@googlemail.com>
2014-08-21 14:11:55 +0100

    Running art/Connect4Main.py

--- art/Connect4Main.py
+++ art/Connect4Main.py
@@ -1,6 +1,6 @@
 import github.BasRegi.art.connect4_grid as grid
 from github.andyandywells.matrixFunctions import *
 from github.GriffithsBen.art.DetGrid import *
-matrixFunctions.matrixFunctions()
+
 grid.drawGrid() 
 
