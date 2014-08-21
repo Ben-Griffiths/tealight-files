bd1c13858895b9b9ffb1437560f6b91bd8c1d806
GriffithsBen <skulled590@googlemail.com>
2014-08-21 14:24:39 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -1,5 +1,5 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
-from github.andyandywells.
+from github.andyandywells.matrixFunctions import *
 from github.griffithsben.art.connect4_defs import *
 
 matrix = initialiseMatrix()
@@ -11,6 +11,7 @@ def handle_mousedown(x,y):
   Array_Y = (y - offset_y) // cell_size
   CentreX = (Array_X+1)*100+75
   CentreY = (2+Array_Y)*100 +50
+  print matrix
   if (Pturn == True):
     Pturn = False
     if(CentreX != 0):         
@@ -24,7 +25,7 @@ def handle_mousedown(x,y):
   spot(CentreX-1,CentreY-1, 35)
  # print (CentreX,CentreY,Array_X,Array_Y)
   
-if (gridarray[i,j-1] != 0):
-  update colour and array
-else
-  set up a goto
+#if (gridarray[i,j-1] != 0):
+#  update colour and array
+#else
+#  set up a goto
