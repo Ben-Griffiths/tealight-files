3fa85b50a10825d0512b03b2f68632219a3f8450
GriffithsBen <skulled590@googlemail.com>
2014-08-21 10:57:00 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -1,1 +1,14 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+Array_X = 0
+Array_Y = 0
+def handle_mousedown(x,y):
+  global Array_X
+  global Array_y
+  spot(x,y,10)
+  Array_X = x
+  Array_Y = y
+  print x
+  print y
+
+  
+  
