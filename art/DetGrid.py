2fc203379f3e3645cd8375a7b43c44bbe9678fdb
GriffithsBen <skulled590@googlemail.com>
2014-08-21 11:24:05 +0100

    Closing art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -9,13 +9,27 @@ def handle_mousedown(x,y):
   Array_Y = y
   print x
   print y
-
-#if(x >= 0 and x <= 50):
-#
-#  #array x coord is set as X
-
+  
+  if(x >= 125 and x < 225):
+    print 1
+  elif(x >= 225 and x < 325):
+    print 2
+  elif(x >= 325 and x < 425):
+    print 3
+  elif(x >= 425 and x < 525):
+     print 4
+  elif(x >= 525 and x < 625):
+    print 5
+  elif(x >= 625 and x < 725):
+    print 6
+  elif(x >= 725 and x < 825):
+    print 7
+    
+    
   
   
+
+
   
   
   
