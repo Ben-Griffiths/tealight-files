f8c0c0b52580d503a59ebcda6f51b79aa8a0a20a
GriffithsBen <skulled590@googlemail.com>
2014-08-21 14:43:03 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -9,6 +9,10 @@ def handle_mousedown(x,y):
   global Pturn
   Array_X = (x - offset_x) // cell_size
   Array_Y = (y - offset_y) // cell_size
+  if(Array_X >=7 or Array_X <0 or Array_Y <0  or Array_Y >= 7):
+    print "test"
+    return 0
+    
   CentreX = (Array_X+1)*100+75
   CentreY = (2+Array_Y)*100 +50  
   if (Pturn == 1):    
