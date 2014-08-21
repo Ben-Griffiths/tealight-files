6ba7e2356ad6eeb4acb987ad7b5c3455851a56a1
GriffithsBen <skulled590@googlemail.com>
2014-08-21 11:30:09 +0100

    Closing art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -1,7 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 Array_X = 0
 Array_Y = 0
-Pturn = true
+#Pturn = True
 def handle_mousedown(x,y):
   global Array_X
   global Array_y
