b1d0c21c51b06ab94dab52ea5de112987592acf2
GriffithsBen <skulled590@googlemail.com>
2014-08-21 14:24:01 +0100

    Closing art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -1,5 +1,5 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
-
+from github.andyandywells.
 from github.griffithsben.art.connect4_defs import *
 
 matrix = initialiseMatrix()
