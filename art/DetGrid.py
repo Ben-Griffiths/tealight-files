da471991d20a4447d64a3b771252c5c90163b063
GriffithsBen <skulled590@googlemail.com>
2014-08-21 14:46:21 +0100

    Closing art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -10,7 +10,6 @@ def handle_mousedown(x,y):
   Array_X = (x - offset_x) // cell_size
   Array_Y = (y - offset_y) // cell_size
   if(Array_X >=7 or Array_X <0 or Array_Y <0  or Array_Y >= 7):
-    print "test"
     return 0
     
   CentreX = (Array_X+1)*100+75
@@ -18,19 +17,25 @@ def handle_mousedown(x,y):
   if (Pturn == 1):    
     if(CentreX != 0):         
       
-      matrix[Array_X][Array_Y] = Pturn
+      #matrix[Array_X][Array_Y] = Pturn
       print matrix
       color("red")
       Pturn = -1    
   else:
     if(CentreX != 0):        
-      matrix[Array_X][Array_Y] = Pturn
+      #matrix[Array_X][Array_Y] = Pturn
       print matrix
       color("yellow")
       Pturn = 1
       
   spot(CentreX-1,CentreY-1, 35)
- # print (CentreX,CentreY,Array_X,Array_Y)
+
+  if(matrix[Array_X][Array_Y-1] !=0
+     
+  for Matrix_Y in range (0,6):
+    
+  
+  # print (CentreX,CentreY,Array_X,Array_Y)
   
 #if (gridarray[i,j-1] != 0):
 #  update colour and array
