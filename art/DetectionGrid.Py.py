b3c45518634ce29b7f78a7c242980f86ffed8eb8
Ben-Griffiths <skulled590@googlemail.com>
2014-08-21 10:46:33 +0100

    Running art/DetectionGrid.Py.py

--- art/DetectionGrid.Py.py
+++ art/DetectionGrid.Py.py
@@ -1,10 +1,7 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 def handle_mousedown(x,y):
-  #get coords
-  #global x
-  #g#lobal y
   spot(x,y,10)
 
   
-  #Detection_Grid()
+  
