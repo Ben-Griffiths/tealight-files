662dbde42d6ff0491fa5ae354e2e732d285b9d28
GriffithsBen <skulled590@googlemail.com>
2014-08-22 09:31:11 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -24,7 +24,7 @@ def handle_mousedown(x,y):
     color("yellow")
     Pturn = 1     
   #for P in range (7,0):
-  if (matrix[Array_Y][Array_X] == 0):
+  if (matrix[Array_Y][Array_X-1] == 0):
     # if matrix[Array_X][Array_Y] == 0:
     #CentreY = (2+P)*100 + 50
     spot(CentreX, CentreY, 35)
