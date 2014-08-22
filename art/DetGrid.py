aa4d12ce00220d3b34c034ddabf6f174e5f2672a
GriffithsBen <skulled590@googlemail.com>
2014-08-22 09:03:54 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -8,15 +8,16 @@ matrix = initialiseMatrix()
 Pturn = 1
 def handle_mousedown(x,y):
   global Pturn
-  Array_X = (x - offset_x) // cell_size
-  Array_Y = (y - offset_y) // cell_size
+  Array_X = (x - offset_x) // cell_size #sets xcoord for array from click
+  Array_Y = (y - offset_y) // cell_size #sets ycoord for array from click
   if(Array_X >=7 or Array_X <0 or Array_Y <0  or Array_Y >= 7):
-    return 0
+    return 0 #breaks function if outside of grid without switching
     
-  CentreX = (Array_X+1)*100+75
-  CentreY = (2+Array_Y)*100 +50  
+  CentreX = (Array_X+1)*100+75 #finds centre point for dot as below
+  CentreY = (2+Array_Y)*100 +50 
+  
   for y in range (6,0):
-    if matrix[Array_X][y] == 0:
+    if matrix[Array_X][] == 0:
       if (Pturn == 1): 
         color("red")
         Pturn = -1 
@@ -26,7 +27,7 @@ def handle_mousedown(x,y):
     
       if(CentreX != 0):         
        # if (matrix[Array_X][Array_Y] == 0):
-         if (matrix[Array_X][y] == 0):
+         if (matrix[Array_X][Array_Y] == 0):
           spot(CentreX-1,CentreY-1, 35)
           matrix[Array_X][Array_Y] = Pturn
           print matrix
