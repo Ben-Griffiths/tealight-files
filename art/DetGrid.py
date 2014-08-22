592dcc381e0eb948939ba5528274da01d4d34100
GriffithsBen <skulled590@googlemail.com>
2014-08-22 09:39:29 +0100

    Running art/DetGrid.py

--- art/DetGrid.py
+++ art/DetGrid.py
@@ -17,8 +17,8 @@ def handle_mousedown(x,y):
   CentreY = (2+Array_Y)*100 +50 
   
   
-  if (matrix[Array_Y][Array_X-1] != 0 or (Array_X-1) == -1):
-      if matrix[Array_Y][Array_X] == 0: #!= 1 or matrix[Array_Y][Array_X] != -1:
+  if (matrix[Array_X][Array_Y-1] != 0 or (Array_X-1) == -1):
+      if matrix[Array_X][Array_Y] == 0: #!= 1 or matrix[Array_Y][Array_X] != -1:
         if Pturn == 1:
           color("red")
           Pturn = -1
@@ -27,11 +27,11 @@ def handle_mousedown(x,y):
           Pturn = 1     
         spot(CentreX, CentreY, 35)
         matrix[Array_Y][Array_X] = -Pturn
+        print(checkwin(matrix))
         return 0
   
   
-  print(checkwin(matrix))
-  
+ 
   
   
   
